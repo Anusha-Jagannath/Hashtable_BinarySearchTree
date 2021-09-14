411118053dccea733a81d007cb49ae6f06a0a370
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-14 14:53:47 +0530

    Added: node interface

---
+++ src/com/hashtable/MyNode.java
@@ -0,0 +1,26 @@
+package com.hashtable;
+
+public class MyNode<K,V> implements INode<K> {
+
+	private K key;
+	private V value;
+	@Override
+	public K getKey() {
+		return null;
+	}
+	@Override
+	public void setKey(K key) {
+		
+	}
+	@Override
+	public INode<K> getNext() {
+		return null;
+	}
+	@Override
+	public void setNext(INode<K> next) {
+		
+	}
+	
+	
+
+}
